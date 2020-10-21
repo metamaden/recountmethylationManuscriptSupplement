3ab1a6e6b5d41fabdb60c1f64286a1cba98e7dda
Sean <maden@ohsu.edu>
2020-10-21 08:49:00 -0700

    adds blocking to mdmod script
    adds getblocks(), block processing for model predictions script.

--- inst/scripts/metadata/metadata_model_predictions.R
+++ inst/scripts/metadata/metadata_model_predictions.R
@@ -10,9 +10,11 @@
 # appended with other mined and learned metadata in the final sample 
 # metadata table (Table S1).
 
+library(recountmethylationManuscriptSupplement)
+library(HDF5Array)
 library(recountmethylation)
-library(wateRmelon)
-library(minfi)
+library(wateRmelon) # for age predictions
+library(minfi) # for cell type and sex predictions
 
 #----------
 # load data
@@ -24,28 +26,49 @@ md.fn <- "metadata_preprocessing.rda"
 mdpre <- get(load(file.path(md.dir, md.fn)))
 
 # download and load the H5SE red/grn signals dataset
-rgset <- recountmethylation::getdb_h5se_rg()
-gmset <- recountmethylation::getdb_h5se_gm()
-grset <- recountmethylation::getdb_h5se_gr()
+#rgset <- recountmethylation::getdb_h5se_rg()
+#grset <- recountmethylation::getdb_h5se_gr()
+
+# load the delayed arrray databases
+rgset.path <- "remethdb_h5se-rg_0-0-1_1590090412"
+grset.path <- "remethdb-h5se_gr_0-0-1_1590090412"
+rgset <- loadHDF5SummarizedExperiment(rgset.path)
+grset <- loadHDF5SummarizedExperiment(grset.path)
 
 #----------------
 # get predictions
 #----------------
-# sex predictions
-sexpred <- getSex(gmset)
-# cell type predictions
-celltypepred <- estimateCellCounts(rgset)
-# age predictions
-predage <- agep(getBeta(grset))
+# start mdmod
+mdmod <- matrix(nrow = 0, ncol = 8)
 
-#--------------------
-# make and save table
-#--------------------
+# do pred on sample index blocks
+blocks <- getblocks(ncol(rgset), 50) # get input indices
+for(b in blocks){
+  rgf <- rgset[, unlist(b)]
+  rgf.matrix <- RGChannelSet(Green = as.matrix(getGreen(rgf)),
+                             Red = as.matrix(getRed(rgf)),
+                             annotation = annotation(rgf))
+  celltypepred <- estimateCellCounts(rgf.matrix) # cell type predictions
+  msf <- mapToGenome(preprocessRaw(rgf))
+  sexpred <- getSex(msf) # sex predictions
+  grf <- grset[,colnames(rgf)]
+  predage <- agep(getBeta(grf)) # age predictions
+  # append results
+  mdf <- cbind(predage, cbind(sexpred[,3], celltypepred))
+  mdmod <- rbind(mdmod, mdf)
+  message("finished up to sample index ", max(unlist(b)))
+}
+
+# format table
+mdmod <- as.data.frame(mdmod, stringsAsFactors = FALSE)
+colnames(mdmod) <- c("predage", "predsex", 
+                     paste0("predcell.", colnames(mdmod)[3:8]))
+mdmod$gsm <- gsub("\\..*", "", rownames(mdmod))
+mdmod <- mdmod[!duplicated(mdmod$gsm),]
+
+#-----
+# save
+#-----
 table.fn <- "mdmod"
-mdmod <- data.frame(gsm = rownames(predage))
-mdmod <- cbind(mdmod, cbind(sexpred[,3], cbind(predage, celltypepred)))
-colnames(mdmod) <- c("gsm", "predsex", "predage", 
-                     paste0("predcell.", colnames(mdmod)[4:9]))
-
 save(mdmod, file = paste0(table.fn, ".rda"))
 write.csv(mdmod, file = paste0(table.fn, ".csv"))
